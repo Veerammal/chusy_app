6be21752db6901f56f05b24273806175edf09011
Praveenkumar Senthilkumar <2k19cse069@kiot.ac.in>
2022-10-10 22:17:31 +0530

    slider updated

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,63 +6,7 @@ import React from "react";
 const Home = () => {
   return (
     <>
-      {/* "linear-gradient(to bottom right, #FDF8FD, #C5E6F6)" */}
-
-      {/* <Container style={{
-          //paddingTop: "20px",
-          width: "100%",
-          height: "100vh",
-          display: "flex",
-          alignItems: "center",
-          backgroundImage: "linear-gradient(to top left, #FDF, #C5E)",
-        }}>
-
-         
-
-          <Grid container spacing={4}>
-            <Grid item xs={12} sm={6}>
-              <Typography
-                style={{
-                  fontSize: "38px",
-                  marginBottom: "20px",
-                }}
-                variant="h1"
-              >
-                Welcome to Chusy
-              </Typography>
-              
-                <Button
-                component = {Link}
-                to = "/shop"
-                  style={{
-                    textDecoration: "none",
-                    color: "#000000",
-                    backgroundColor: "#03dac5",
-                  }}
-                >
-                  Shop Now
-                </Button>
-              
-            </Grid>
-            <Grid
-              style={{
-                display: "flex",
-                alignItems: "center",
-              }}
-              item
-              sm={6}
-            >
-              <img
-                style={{
-                  width: "100%",
-                }}
-                src={logo}
-                alt="logo"
-              />
-            </Grid>
-          </Grid>
-        </Container> */}
-
+     
       <Slider />
 
       <Divider />
@@ -76,3 +20,60 @@ const Home = () => {
 };
 
 export default Home;
+
+//  {/* "linear-gradient(to bottom right, #FDF8FD, #C5E6F6)" */}
+
+//       {/* <Container style={{
+//           //paddingTop: "20px",
+//           width: "100%",
+//           height: "100vh",
+//           display: "flex",
+//           alignItems: "center",
+//           backgroundImage: "linear-gradient(to top left, #FDF, #C5E)",
+//         }}>
+
+         
+
+//           <Grid container spacing={4}>
+//             <Grid item xs={12} sm={6}>
+//               <Typography
+//                 style={{
+//                   fontSize: "38px",
+//                   marginBottom: "20px",
+//                 }}
+//                 variant="h1"
+//               >
+//                 Welcome to Chusy
+//               </Typography>
+              
+//                 <Button
+//                 component = {Link}
+//                 to = "/shop"
+//                   style={{
+//                     textDecoration: "none",
+//                     color: "#000000",
+//                     backgroundColor: "#03dac5",
+//                   }}
+//                 >
+//                   Shop Now
+//                 </Button>
+              
+//             </Grid>
+//             <Grid
+//               style={{
+//                 display: "flex",
+//                 alignItems: "center",
+//               }}
+//               item
+//               sm={6}
+//             >
+//               <img
+//                 style={{
+//                   width: "100%",
+//                 }}
+//                 src={logo}
+//                 alt="logo"
+//               />
+//             </Grid>
+//           </Grid>
+//         </Container> */}
